6f6ba5078c1cd573c42b5d040abd10b0273bfdb1
Lisa <lb.broadhead@gmail.com>
2022-06-17 11:42:32 -0500

    first commit

--- chapter1/1.1-is_unique.js
+++ chapter1/1.1-is_unique.js
@@ -1,11 +1,23 @@
-// Implament an alogritum to determine if a string has all unique characters. 
-// What if you cannot use additionl data structures?
+// 1. Implament an alogritum to determine if a string has all unique characters. 
 
 const isUnique = (string) => {
+    string = string.toLowerCase()
     const alpah = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
-    console.log(string)
-    console.log(alpah.length)
-
+    
+    for(var i=0; i<string.length;i++){
+        console.log(string.charAt(i))
+        if(alpah[string.charAt(i)] == ' '){
+            continue;
+        }else if(alpah[string.charAt(i)] == 0 ){
+            alpah[string.charAt(i)] = 1 
+        }else{
+            return false;
+        }
+        return true;
+        console.log(string.charAt(i))
+    }
 }
 
-console.log(isUnique('this end fracked you'))
+console.log(isUnique('yOu fRack this end'))
+
+// 2. What if you cannot use additionl data structures?
