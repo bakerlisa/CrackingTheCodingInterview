8f17fb6693ef2e351c19de767c1b9f040d6858d0
Lisa <lb.broadhead@gmail.com>
2022-06-17 12:17:23 -0500

    first commit

---
+++ chapter1: strings and arrays/1.2-check_permutation.js
@@ -0,0 +1,7 @@
+// Given two strings, write a method to decide if one is permutation of the other
+
+const permutation = (str1, str2) => {
+    
+}
+
+console.log(permutation("I'm a string","Hey! So am I!"))
