f136b910c94bfb61ac36db7d30e53fe08d01dab1
Lisa <lb.broadhead@gmail.com>
2022-06-22 14:21:24 -0500

    first commit

--- chapter1: strings and arrays/1.4-Palindrome_Permutation.js
+++ chapter1: strings and arrays/1.4-Palindrome_Permutation.js
@@ -3,16 +3,22 @@
 // Plaindrome: It's tje same backwards as it is forwards: taco cat
 
 const palindrome = (pal,str) => {
-    pal = pal.replace(" ", "").toLowerCase()
-    str = str.replace(" ", "").toLowerCase()
+    // removes sapces, and all unique characters
+    pal = pal.replace(" ", "").replace(/[^a-z0-9]/gi, '').toLowerCase()
+    str = str.replace(" ", "").replace(/[^a-z0-9]/gi, '').toLowerCase()
+    
     if(pal.length != str.length ){
         return false
     }else{
-        for(var i=0;i<pal.length;i++)
+        var runner = pal.length -1;
+        for(var i=0;i<Math.ceil(pal.length);i++){
+            if(pal[i] != str[runner]){
+                return false;
+            }
+            runner--;
+        }
+        return true
     }
-
-    
-
 }
 
-console.log(palindrome("Tact Coa", "aoct Cat"));
+console.log(palindrome("Tact Coa!", "aoct Cat!!!"));
