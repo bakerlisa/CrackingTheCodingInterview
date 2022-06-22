35f3d5bfb80bfa7ec10aca8ebaded437d5709d34
Lisa <lb.broadhead@gmail.com>
2022-06-22 13:45:52 -0500

    first commit

--- chapter1: strings and arrays/1.2-check_permutation.js
+++ chapter1: strings and arrays/1.2-check_permutation.js
@@ -2,10 +2,31 @@
 // permutation : Two strings are a permutatuin if they have the same characters, but in a different order
 
 const permutation = (str1, str2) => {
-    console.log(str1)
-    console.log(str2)
+    const str = 'A!@#b$%^c&*(';
+    str1 =  str1.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    str2 =  str2.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const loader = {}
+
+    for(var i=0; i<str1.length;i++){
+        if(loader[str1[i]]){
+            loader[str1[i]] = loader[str1[i]]+1
+        }else{
+            loader[str1[i]] = 1
+        }
+    }
+    
+    for(var j=0; j<str2.length;j++){
+        if(!loader[str2[j]]){
+            return false
+        }else if(loader[str2[j]] && loader[str2[j]]-1 >= 0){
+            loader[str2[j]] = loader[str2[j]]-1
+        }else{
+            return false
+        }
+    }
+    return true
 }
 
-console.log(permutation("I'm a string!", "mat is ring!"))
+console.log(permutation("I'm a string!", "mat is fring!"))
 //,r,i,n,g
 // mat is ring
