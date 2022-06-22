da818f0a52e660dd132aa6aed30503d0f0eba0c7
Lisa <lb.broadhead@gmail.com>
2022-06-22 14:34:41 -0500

    first commit

---
+++ chapter1: strings and arrays/1.6-String_Compression.js
@@ -0,0 +1,9 @@
+// 1.6 String Compression
+// Implament a method to perform a vasic string compression using counts of repeated characters: aabcccccaaa -> a2b1c5a3
+// You can assume the string is only upper and lowercase numbers
+
+const stringCompression = (str) => {
+
+}
+
+console.log(stringCompression('aaabbbcccdddeee'))
