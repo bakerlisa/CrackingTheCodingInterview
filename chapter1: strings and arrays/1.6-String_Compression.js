265802c950bfdc1faa586bd9af3165c58a2512c1
Lisa <lb.broadhead@gmail.com>
2022-07-01 14:56:43 -0500

    first commit

--- chapter1: strings and arrays/1.6-String_Compression.js
+++ chapter1: strings and arrays/1.6-String_Compression.js
@@ -3,13 +3,25 @@
 // You can assume the string is only upper and lowercase numbers
 
 const stringCompression = (str) => {
-    let compressed = {}
     let runner = 0;
+    let counter = 0;
+    let output = ""
 
     for(let i=0;i<str.length;i++){
-        console.log(str[i])
+        if(str[i] === str[runner] || i === 0){
+            console.log(i)
+            counter++;
+        }else{
+            output += str[runner] + "" + counter;
+            counter = 1;
+        }
         runner = i;
     }
+    // GETS THE LAST ITEM IN THE LOOP
+    output += str[runner] + "" + counter;
+
+    return output
 }
 
 console.log(stringCompression('aaabbbcccdddeee'))
+// console.log(stringCompression('aaabbbcaapptdddeee'))
