9c1bf20985b81c3ad34e2fe61e8ab780314ce885
Lisa <lb.broadhead@gmail.com>
2022-07-08 13:50:41 -0500

    first commit

--- chapter1: strings and arrays/1.9-String_Rotation.js
+++ chapter1: strings and arrays/1.9-String_Rotation.js
@@ -1,6 +1,17 @@
 const stringRotation = (st1, st2) => {
-    console.log(st1)
-    console.log(st2)
+    if(st1.length != st2.length){
+        return false
+    }else{
+        console.log("Yes")
+        for(let i=0; i<st1.length;i++){
+            for(let j=0;j< st2.length;j++){
+                if(st1[i] === st2[j]){
+                    console.log(st1[i])
+                }
+            }
+        }
+    }
 }
 // console.log(stringRotation("This is a string about cats!"))
 console.log(stringRotation("waterbottle", "erbottlewat"))
+
