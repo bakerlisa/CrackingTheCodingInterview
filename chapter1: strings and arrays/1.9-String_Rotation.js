65b282e69607d8d6af2d674f6403dd32dc23fbcd
Lisa <lb.broadhead@gmail.com>
2022-07-08 13:43:17 -0500

    first commit

---
+++ chapter1: strings and arrays/1.9-String_Rotation.js
@@ -0,0 +1,6 @@
+const stringRotation = (st1, st2) => {
+    console.log(st1)
+    console.log(st2)
+}
+// console.log(stringRotation("This is a string about cats!"))
+console.log(stringRotation("waterbottle", "erbottlewat"))
