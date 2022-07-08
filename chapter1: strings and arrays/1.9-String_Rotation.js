eb1ad46712625252b0f7292b877c598c94a565a6
Lisa <lb.broadhead@gmail.com>
2022-07-08 14:00:09 -0500

    first commit

--- chapter1: strings and arrays/1.9-String_Rotation.js
+++ chapter1: strings and arrays/1.9-String_Rotation.js
@@ -1,17 +1,20 @@
+// 1.9 String Rotation - Assume you have a method isSubstring which checks if one word is a substring of another. given 2 strings, write a code to check is they're a roration of eachother
+
 const stringRotation = (st1, st2) => {
     if(st1.length != st2.length){
         return false
     }else{
-        console.log("Yes")
         for(let i=0; i<st1.length;i++){
-            for(let j=0;j< st2.length;j++){
-                if(st1[i] === st2[j]){
-                    console.log(st1[i])
-                }
+            if(st1 != st2){
+                let char = st1[0]
+                st1 = st1.substring(1);
+                st1 = st1 + char
+            }else if(st1 === st2){
+                return true;
             }
         }
     }
+    return false
 }
-// console.log(stringRotation("This is a string about cats!"))
 console.log(stringRotation("waterbottle", "erbottlewat"))
 
